226b38a488b8d37785d8630e6eb7598da863ced5
fs5013-furi-sutao <66060684+fs5013-furi-sutao@users.noreply.github.com>
2020-09-11 23:23:47 +0900

    Update SpringjwtApplication.java

--- src/main/java/com/bezkoder/springjwt/SpringjwtApplication.java
+++ src/main/java/com/bezkoder/springjwt/SpringjwtApplication.java
@@ -3,6 +3,9 @@ package com.bezkoder.springjwt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Spring Boot Token based Authentication with Spring Security & JWT
+// https://bezkoder.com/spring-boot-jwt-authentication/
+
 @SpringBootApplication
 public class SpringjwtApplication {
 
